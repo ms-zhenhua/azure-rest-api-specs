3191e1407fd70e95f11f7b0d6df144985426d514
Zhenhua Hu <zhhu@microsoft.com>
2024-11-15 20:24:37 +0800

    test

--- eng/scripts/Armstrong-Validation.ps1
+++ eng/scripts/Armstrong-Validation.ps1
@@ -116,10 +116,11 @@ $hasArmstrongTestResult = $false
 try {
   $response = Get-GitHubIssueComments -RepoOwner $repoOwner -RepoName $repoName -IssueNumber $pullRequestNumber -AuthToken $AuthToken
   for ($i = $response.Length - 1; $i -ge 0; $i--) {
-    LogInfo $response.body
-    if ($response.body -like "*API TEST ERROR REPORT*") {
+    $responseObject = $response[$i]
+    LogInfo $responseObject.body
+    if ($responseObject.body -like "*API TEST ERROR REPORT*") {
       $hasArmstrongTestResult = $true
-      LogInfo "Armstrong Test result is submitted in PR comments: $($response.html_url)."
+      LogInfo "Armstrong Test result is submitted in PR comments: $($responseObject.html_url)."
     }
   }
 }
